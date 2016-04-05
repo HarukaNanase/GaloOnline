ca6ab21d4f162cb7dcaf4765930f9633233965b8
André Soares <andrecaetanosoares@gmail.com>
2016-04-05 17:32:52 +0100

    Read FileVersion to find out recent changes
    V0.02 - Implementation of Socket System in Server.py   - 05/04/2016 02:25 - André Soares
            Account Registation and 'Database'             - 05/04/2016 02:38 - André Soares
            Definition of Error and Success Message        - 05/04/2016 16:35 - André Soares
            Updated Project Socket Library                 - 05/04/2016 16:35 - André Soares
            First communication test                       - 05/04/2016 16:36 - André Soares
            Check for error in register                    - 05/04/2016 16:36 - André Soares
            Register mechanism 95% completed               - 05/04/2016 16:37 - André Soares
            #TODO : Finish LoadAccounts                    - 05/04/2016 16:38 - André Soares

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 import os
 
 #Define
+ServerIP = socket.gethostname()
 Port = 8000
 UsersPath = "./users/"
 FileExtension = ".txt"
-
+SuccessMessage = "ACK"
+ErrorMessage = "ERR"
+MessageMaxSize = 256
 #EndDefine
+
+#Message Encodes
+SuccessMessage = SuccessMessage.encode()
+ErrorMessage = ErrorMessage.encode()
+#
+
+
 def CheckDir():
     if os.path.exists(UsersPath):
         return True
@@ -19,7 +29,7 @@ def CreateServerSocket():
     try:
         ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print("Socket Sucessfully Created!")
-        ServerSocket.bind((socket.gethostname(), Port))
+        ServerSocket.bind((ServerIP, Port))
         print("Socket binded Sucessfully")
         return ServerSocket
     except socket.error:
@@ -29,14 +39,23 @@ def CreateServerSocket():
 def WriteToSocket(ServerSocket, msg, address):
     try:
         ServerSocket.sendto(msg, address)
-        return True
+        data = ServerSocket.recvfrom(256)
+        UserEndPoint = data[1]
+        userMsg = data[0]
+        userMsg = userMsg.split()
+
+        if userMsg[0] == SuccessMessage:
+            return True
+        else:
+            print("Error on receiving ACK from client " + data[1][0])
+            return False
     except socket.error:
         print("Unable to send message to address %s", address)
         return False
 
 
 def ReadFromSocket(ServerSocket):
-    return ServerSocket.recvfrom(256)
+    return ServerSocket.recvfrom(MessageMaxSize)
 
 
 def CreateAccount(name, password, accounts):
@@ -49,13 +68,14 @@ def CreateAccount(name, password, accounts):
             userfile.close()
             accounts[name] = password
             print("Account created!")
-            return accounts
+            #return accounts
         except IOError:
             print("Error opening file.")
             return False
 
-def LoadAccounts():
-    #TODO
+
+def LoadAccounts(Accounts):
+
 
 
 def Login(name, password):
@@ -86,22 +106,23 @@ def main():
     Accounts = {}
     LoggedInUsers = {}
     ServerSocket = CreateServerSocket()
-    print(socket.gethostname())
+    print("Hosting @"+ServerIP)
     while 1:
         Data = ReadFromSocket(ServerSocket)
         UserIP = Data[1]
         OpCode = Data[0].decode()
-        print(OpCode)
+        print("Mensagem recebida: %s",OpCode)
         OpCode = OpCode.split()
-        print(OpCode[0])
-        print(OpCode[1])
-        print(OpCode[2])
+        # print(OpCode[0])
+        # print(OpCode[1])
+        # print(OpCode[2])
         if OpCode[0] == "REG":
-            Sucess = CreateAccount(OpCode[1], OpCode[2], Accounts)
-            if Sucess == False:
+            Success = CreateAccount(OpCode[1], OpCode[2], Accounts)
+            if Success == False:
                 print("Account creation failed. Now continuing.")
-                continue
+                WriteToSocket(ServerSocket, ErrorMessage, UserIP)
             else:
+                WriteToSocket(ServerSocket, SuccessMessage, UserIP)
                 #Accounts = Sucess
                 print(Accounts)
         else:
